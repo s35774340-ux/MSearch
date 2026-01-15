a8441c2173626f282d91b0cfeeb7cc387dde0f88
Step4ik <s35774340@gmail.com>
2026-01-15 18:36:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+﻿from MSearch import MSearch
+
+def main(db_file):
+    array = []
+    with open(f"{db_file}.txt", "r", encoding="utf-8") as r:
+        for line in r:
+            array.append(line)
+    MSearch(array)
+            
+    
